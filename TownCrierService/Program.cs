9c132c3925e0caaff0a38250fd1bc6d7beb980b8
Fabio Scopel <fabiobscopel@gmail.com>
2020-08-17 19:18:42 -0500

    initial commit

---
+++ TownCrierService/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Topshelf;
+
+namespace TownCrierService
+{
+   
+
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+            var rc = HostFactory.Run(x =>                                   //1
+            {
+                x.Service<TownCrierService>(s =>                                   //2
+                {
+                    s.ConstructUsing(name => new TownCrierService());                //3
+                    s.WhenStarted(tc => tc.FooStart());                         //4
+                    s.WhenStopped(tc => tc.BarStop());                          //5
+                });
+                x.RunAsLocalSystem();                                       //6
+
+                x.SetDescription("Town Crier Service description goes here.");                   //7
+                x.SetDisplayName("Town Crier Service");                                  //8
+                x.SetServiceName("TownCrierService");                                  //9
+            });                                                             //10
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());  //11
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
---
+++ TownCrierService/TownCrierService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Timers;
+
+namespace TownCrierService
+{
+    public class TownCrierService
+    {
+        readonly Timer _timer;
+        public TownCrierService()
+        {
+            _timer = new Timer(1000) { AutoReset = true };
+#if DEBUG
+            _timer.Elapsed += (sender, eventArgs) => Console.WriteLine("It is {0} and all is well", DateTime.Now);
+#else
+            _timer.Elapsed += (sender, eventArgs) => File.AppendAllLines(@"c:\temp\towncrier.log", new string[] { string.Format("It is {0} and all is well", DateTime.Now) });
+#endif
+        }
+
+        public void FooStart() { _timer.Start(); }
+        
+        public void BarStop() { _timer.Stop(); }
+    }
+}
